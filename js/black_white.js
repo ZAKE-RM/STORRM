e09df4b5aa7987df4ed047096252878be804ff51
ZAKE-RM <thinckall@gmail.com>
2024-07-07 12:11:49 +0100

    Add files via upload

---
+++ js/black_white.js
@@ -0,0 +1,94 @@
+$(document).ready(function(){
+    $('#b1').click(function(){
+    $('body').css({
+                      'color':'rgb(247, 239, 239)',
+                      'background':'black',
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b1').click(function(){
+    $('.card').css({  
+                      'background':'black' ,
+                      'box-shadow': '0 5px 25px rgb(255 255 255 /15%)',     
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b1').click(function(){
+    $('.about').css({
+                      'background':'rgb(17, 17, 17)',            
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b1').click(function(){
+    $('.info h3').css({  
+        'color':'rgb(247, 239, 239)'   
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b1').click(function(){
+    $('.info p').css({  
+        'color':'rgb(247, 239, 239)'   
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('body').css({
+                      'color':'black',
+                      'background':'rgb(247, 239, 239)'
+                     
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('.card').css({
+                      'color':'black',
+                      'background':'rgb(247, 239, 239)',
+                      'box-shadow': '0 5px 25px rgb(1 1 1 /15%)',             
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('.info h3').css({  
+        'color':'black'   
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('.info p').css({  
+        'color':'black'   
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('.about').css({
+                      'background':'rgb(85, 83, 83)',            
+    });
+    })
+    });
+    
+    $(document).ready(function(){
+    $('#b2').click(function(){
+    $('.about .container-texts p').css({
+                      'color':'rgb(255, 255, 255)'           
+    });
+    })
+    });
+    
